aff8984029a680c63f73e7c44e51af971d341a0a
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-08 01:57:30 +0300

    Create login endpoint

--- src/api/routes/auth.py
+++ src/api/routes/auth.py
@@ -4,10 +4,10 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from src.core.security import hash_password
+from src.core.security import hash_password, verify_password, create_access_token
 from src.db.models.user import User
 from src.db.session import SessionLocal
-from src.schemas.user import UserCreate, UserRead
+from src.schemas.user import UserCreate, UserRead, Token
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
 
@@ -39,4 +39,18 @@ def register(user_in: UserCreate, db: Session = Depends(get_db)):
 
     return new_user
 
+@router.post('/login', response_model=Token)
+def login(user_in: UserCreate, db: Session = Depends(get_db)):
+    result = db.execute(select(User).where(User.username == user_in.username))
+    user = result.scalar_one_or_none()
 
+    if not user or not verify_password(user_in.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Incorrect username or password',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+
+    access_token = create_access_token(subject=user.id)
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
