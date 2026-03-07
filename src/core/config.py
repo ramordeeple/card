40e42a7680ba8ca6e2078f3260f64374aea29728
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-08 02:18:41 +0300

    Relate token expiration to .env

--- src/core/config.py
+++ src/core/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str
+    ADMIN:str
+    ADMIN_PASSWORD: str
 
     class Config:
         env_file = '.env.example'
