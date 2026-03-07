202ddacc37a4f870da4c0301404c5358f2e1161b
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-08 02:26:58 +0300

    Add encryption key

--- src/core/config.py
+++ src/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     ADMIN:str
     ADMIN_PASSWORD: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ENCRYPTION_KEY: str
 
     class Config:
         env_file = '.env.example'
