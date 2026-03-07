3568c669738f8ef1fbb7e061b602614d5f30726c
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-08 00:45:31 +0300

    Refactor with adding sqlalchemy alias for avoiding confusing and table name to cards from card

--- src/db/models/card.py
+++ src/db/models/card.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import date
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import ForeignKey, String, DateTime, Enum
+from sqlalchemy import ForeignKey, String, DateTime, Enum as sqlalchemy_enum
 from sqlalchemy.orm import Mapped
 from sqlalchemy.testing.schema import mapped_column
 
@@ -10,7 +10,7 @@ from src.domain.enums.card_status import CardStatus
 
 
 class Card(Base):
-    __tablename__ = 'card'
+    __tablename__ = 'cards'
 
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
@@ -21,7 +21,7 @@ class Card(Base):
     owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('user.id'), nullable=False)
     expiration_date: Mapped[date] = mapped_column(DateTime, nullable=False)
     status: Mapped[int] = mapped_column(
-        Enum(CardStatus),
+        sqlalchemy_enum(CardStatus),
         default=CardStatus.ACTIVE,
         nullable=False
     )
