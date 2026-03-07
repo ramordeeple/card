6ce4c55bb0216ec0e190649ce1e0f0097a0bed24
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-07 21:18:01 +0300

    Add db config with env

---
+++ src/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+
+    class Config:
+        env_file = ".env.example"
+
+settings = Settings()
