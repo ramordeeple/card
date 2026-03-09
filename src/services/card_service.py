81f4cc0ed9a41b5f68893a45c04a155fa7db2591
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-09 18:22:53 +0300

    Add 2 services for cards

---
+++ src/services/card_service.py
@@ -0,0 +1,42 @@
+from decimal import Decimal
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
+from src.core.security.encrypt_card_number import encrypt_card_number, decrypt_card_number
+from src.db.models.card import Card
+from src.domain.rules import card_rules
+from src.schemas.card import CardCreate
+
+
+class CardService:
+    @staticmethod
+    def create_card(db: Session, card_in: CardCreate) -> Card:
+        encrypted_bytes = encrypt_card_number(card_in.number)
+
+        new_card = Card(
+            owner_id=card_in.owner_id,
+            number_encrypted=encrypted_bytes,
+            balance=Decimal(0.00)
+        )
+
+        db.add(new_card)
+        db.commit()
+        db.refresh(new_card)
+
+        return new_card
+
+    @staticmethod
+    def get_card_for_display(db: Session, card_id: UUID) -> dict:
+        card = db.query(Card).filter(Card.id == card_id).first()
+        if not card:
+            return None
+
+        raw_number = decrypt_card_number(card.number_encrypted)
+        masked = card_rules.get_masked_card_number(raw_number)
+
+        return {
+            "id": card.id,
+            "balance": card.balance,
+            "number_masked": masked,
+        }
