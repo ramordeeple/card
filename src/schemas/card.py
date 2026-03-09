232088438c60c7a3246d9b23b1c26c0043114f41
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-09 17:54:56 +0300

    Add card create

--- src/schemas/card.py
+++ src/schemas/card.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 
 from pydantic import BaseModel, ConfigDict
 
+from src.domain.rules.card_rules import CardNumberLength
+
 
 class CardRead(BaseModel):
     id: uuid.UUID
@@ -10,3 +12,7 @@ class CardRead(BaseModel):
     number_masked: str
 
     model_config = ConfigDict(from_attributes=True)
+
+class CardCreate(BaseModel):
+    owner_id: uuid.UUID
+    number: CardNumberLength
