943c2deaf41c7c91e3d5e587a973a7f4344d9e6f
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-11 00:14:25 +0300

    Refactor change paths to constants

--- src/schemas/card.py
+++ src/schemas/card.py
@@ -4,13 +4,13 @@ from typing import Annotated
 
 from pydantic import BaseModel, ConfigDict, Field
 
-from src.db.models import card
+from src.domain.constants import card_constants
 
 CardNumberLength = Annotated[
     str,
     Field(
-        min_length=card.CARD_NUMBER_LENGTH,
-        max_length=card.CARD_NUMBER_LENGTH,
+        min_length=card_constants.CARD_NUMBER_LENGTH,
+        max_length=card_constants.CARD_NUMBER_LENGTH,
         pattern=r"^\d+$"
     )
 ]
