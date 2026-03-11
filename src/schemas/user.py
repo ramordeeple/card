5a0a42b2d734108bf6d4522f352d0abad5357f02
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-11 15:26:34 +0300

    Fix missing n in token

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -18,5 +18,5 @@ class UserRead(UserBase):
     model_config = ConfigDict(from_attributes=True)
 
 class Token(BaseModel):
-    access_toke: str
+    access_token: str
     token_type: str = 'bearer'
