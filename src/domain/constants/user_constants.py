d837484106854cd690033c28abe1897d061edfc6
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-11 14:32:05 +0300

    Refactor add and use new constants

--- src/domain/constants/user_constants.py
+++ src/domain/constants/user_constants.py
@@ -1,4 +1,6 @@
 from typing import Final
 
 USERNAME_MAX_LENGTH: Final[int] = 50
+USERNAME_MIN_LENGTH: Final[int] = 3
 PASSWORD_HASH_MAX_LENGTH: Final[int] = 255
+PASSWORD_MIN_LENGTH: Final[int] = 8
--- src/schemas/user.py
+++ src/schemas/user.py
@@ -2,15 +2,16 @@ import uuid
 
 from pydantic import BaseModel, Field, ConfigDict
 
+from src.domain.constants.user_constants import USERNAME_MIN_LENGTH, USERNAME_MAX_LENGTH, PASSWORD_MIN_LENGTH
 from src.domain.enums.user_role import UserRole
 
 
 class UserBase(BaseModel):
-    username: str = Field(..., min_length=4, max_length=50)
+    username: str = Field(..., min_length=USERNAME_MIN_LENGTH, max_length=USERNAME_MAX_LENGTH)
     role: UserRole = UserRole.USER
 
 class UserCreate(UserBase):
-    password: str = Field(..., min_length=8)
+    password: str = Field(..., min_length=PASSWORD_MIN_LENGTH)
 
 class UserRead(UserBase):
     id: uuid.UUID
