f823cb156992a660062b3a3af33ba95e6d332055
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-11 15:28:01 +0300

    Change max length for hashed pwd to 50

--- src/domain/constants/user_constants.py
+++ src/domain/constants/user_constants.py
@@ -2,5 +2,5 @@ from typing import Final
 
 USERNAME_MAX_LENGTH: Final[int] = 50
 USERNAME_MIN_LENGTH: Final[int] = 3
-PASSWORD_HASH_MAX_LENGTH: Final[int] = 255
+PASSWORD_HASH_MAX_LENGTH: Final[int] = 50
 PASSWORD_MIN_LENGTH: Final[int] = 8
