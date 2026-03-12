8b2c2283a077eaa13271c70e4573e1714437b8b3
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-12 07:10:30 +0300

    Add validity years const

--- src/domain/constants/card_constants.py
+++ src/domain/constants/card_constants.py
@@ -5,3 +5,4 @@ CARD_MASK_VISIBLE_END: Final[int] = 4
 CARD_MASK_CHAR: Final[str] = '*'
 CARD_ENCRYPTED_MAX_LENGTH = 512
 CARD_MASK_TEMPLATE: Final[str] = "**** **** **** "
+CARD_VALIDITY_YEARS: Final[int] = 5
