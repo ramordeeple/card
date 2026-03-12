dc83fee703d1709a10ccb77017d965a71791acd2
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-12 07:17:01 +0300

    Refactor quotes

--- src/domain/constants/card_constants.py
+++ src/domain/constants/card_constants.py
@@ -4,5 +4,5 @@ CARD_NUMBER_LENGTH: Final[int] = 16
 CARD_MASK_VISIBLE_END: Final[int] = 4
 CARD_MASK_CHAR: Final[str] = '*'
 CARD_ENCRYPTED_MAX_LENGTH = 512
-CARD_MASK_TEMPLATE: Final[str] = "**** **** **** "
+CARD_MASK_TEMPLATE: Final[str] = '**** **** **** '
 CARD_VALIDITY_YEARS: Final[int] = 5
