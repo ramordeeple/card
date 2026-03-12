d05c1f65915bc581731a154d3127c0a669e0c898
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-12 20:24:32 +0300

    Refactor transaction service dividing db out of it

--- src/services/transaction_service.py
+++ src/services/transaction_service.py
@@ -1,58 +1,33 @@
 from decimal import Decimal
 from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status  # Импортируем status
+from sqlalchemy.sql import crud
 
 from src.db.models.card import Card
 
 
 class TransactionService:
     @staticmethod
-    def transfer_money(
-        db: Session, from_card_id: UUID, to_card_id: UUID, amount: Decimal
-    ):
+    async def transfer_money(db: AsyncSession, from_id: UUID, to_id: UUID, amount: Decimal, owner_id: UUID):
         if amount <= Decimal('0.00'):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Сумма перевода должна быть положительной',
-            )
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Transaction cannot be negative or equal to zero')
 
-        try:
-            # For avoiding deadlocks
-            ids = sorted([from_card_id, to_card_id])
+        ids = sorted([from_id, to_id])
+        cards = await crud.get_cards(db, ids, owner_id)
 
-            cards_query = (
-                db.query(Card).filter(Card.id.in_(ids)).with_for_update().all()
-            )
-            cards_map = {card.id: card for card in cards_query}
+        if len(cards) < 2:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Cards not found')
 
-            if len(cards_map) < 2:
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail="Sender's card or receiver's not found",
-                )
+        cards_map = {card.id: card for card in cards}
+        sender, receiver = cards_map[from_id], cards_map[to_id]
 
-            sender = cards_map[from_card_id]
-            receiver = cards_map[to_card_id]
+        if sender.balance < amount:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Insufficient funds')
 
-            if sender.balance < amount:
-                raise HTTPException(
-                    status_code=status.HTTP_402_PAYMENT_REQUIRED,
-                    detail='Insufficient funds',
-                )
+        sender.balance -= amount
+        receiver.balance += amount
 
-            sender.balance -= amount
-            receiver.balance += amount
-
-            db.commit()
-            return {'status': 'success', 'amount': amount}
-
-        except Exception as e:
-            db.rollback()
-            if isinstance(e, HTTPException):
-                raise e
-            # Вместо 500
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail='Internal Server Error',
-            )
+        return sender
