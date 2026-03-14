3f5444a7d9b96814d61405e87616deea9bcff87c
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-15 01:07:15 +0300

    Add admin get and make async get db

--- src/api/deps.py
+++ src/api/deps.py
@@ -2,17 +2,18 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.routes.auth import get_db
 from src.core.config import settings
 from src.db.models.user import User
+from src.domain.enums.user_role import UserRole
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 async def get_current_user(
-    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+    db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)
 ) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -40,3 +41,10 @@ async def get_current_user(
         raise credentials_exception
 
     return user
+
+async def get_current_admin(current_user: User = Depends(get_current_user)) -> User:
+    if current_user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+
+    return current_user
+
