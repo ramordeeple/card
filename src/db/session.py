3002b29e5b6db0040b86137a515e1bcecbd6afa6
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-14 08:26:31 +0300

    Refactor session to async

--- src/db/session.py
+++ src/db/session.py
@@ -1,13 +1,25 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
 from src.core.config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL, pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
 class Base(DeclarativeBase):
     pass
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False
+)
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
