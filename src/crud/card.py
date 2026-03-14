be698224e65f8eb4a0094e30ad0048b7b2774dfe
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-14 10:38:18 +0300

    Rename func

--- src/crud/card.py
+++ src/crud/card.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.models.card import Card
 
 
-async def get_cards_by_ids(db: AsyncSession, card_ids: list[uuid.UUID], owner_id: uuid.UUID):
+async def get_cards(db: AsyncSession, card_ids: list[uuid.UUID], owner_id: uuid.UUID):
     stmt = (
         select(Card)
         .where(Card.id.in_(card_ids), Card.owner_id == owner_id)
