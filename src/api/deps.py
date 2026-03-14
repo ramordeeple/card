d4896719c14951d1945b36b2bf0c208bd85eec3f
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-14 08:25:17 +0300

    Add async getting user

--- src/api/deps.py
+++ src/api/deps.py
@@ -1,36 +1,41 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError
+from jose import jwt, JWTError
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from src.api.routes.auth import get_db
 from src.core.config import settings
-from src.core.security import jwt
 from src.db.models.user import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/login')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-def get_current_user(db: Session = Depends(get_db),
-                     token: str = Depends(oauth2_scheme)) -> User:
+
+async def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail='Could not validate credentials',
-        headers={'WWW-Authenticate': 'Bearer'},
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
 
     try:
-        payload = jwt.decode(token,
-                             settings.JWT_SECRET_KEY,
-                             algorithms=[settings.JWT_ALGORITHM])
+        payload = jwt.decode(
+            token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM]
+        )
 
-        user_id: str = payload.get('sub')
+        user_id: str = payload.get("sub")
         if user_id is None:
             raise credentials_exception
 
     except JWTError:
         raise credentials_exception
 
-    user = db.query(User).filter(User.id == user_id).first()
+    statement = select(User).where(User.id == user_id)
+    result = await db.execute(statement)
+    user = result.scalar_one_or_none()
+
     if user is None:
         raise credentials_exception
 
