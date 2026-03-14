2e4a611be6d21927ecf1c822bfc271822e1a82e7
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-14 08:20:31 +0300

    Refactor card model for number to secure it in db

--- src/db/models/card.py
+++ src/db/models/card.py
@@ -1,13 +1,14 @@
 import uuid
 from datetime import date
+from decimal import Decimal
 from typing import TYPE_CHECKING
 
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import ForeignKey, String, DateTime, Enum as sqlalchemy_enum
+from sqlalchemy import ForeignKey, String, DateTime, Enum as sqlalchemy_enum, Numeric, LargeBinary
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from src.db.session import Base
-from src.domain.constants.card_constants import CARD_ENCRYPTED_MAX_LENGTH
+from src.domain.constants.card_constants import CARD_ENCRYPTED_MAX_LENGTH, CARD_MASK_VISIBLE_END
 from src.domain.enums.card_status import CardStatus
 
 if TYPE_CHECKING:
@@ -21,8 +22,10 @@ class Card(Base):
         primary_key=True,
         default=uuid.uuid4
     )
-    number_encrypted: Mapped[str] = mapped_column(String(CARD_ENCRYPTED_MAX_LENGTH), unique=True, nullable=False)
+    number_encrypted: Mapped[bytes] = mapped_column(LargeBinary, unique=True, nullable=False)
+    number_last4: Mapped[str] = mapped_column(String(CARD_MASK_VISIBLE_END), nullable=False)
     owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.id', ondelete='RESTRICT'), nullable=False)
+    balance: Mapped[Decimal] = mapped_column(Numeric(precision=15, scale=2), default=Decimal('0.00'))
     expiration_date: Mapped[date] = mapped_column(DateTime, nullable=False)
     status: Mapped[int] = mapped_column(
         sqlalchemy_enum(CardStatus),
