3148d8445416b7e3057c1a8881228321cf737b4c
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-14 11:07:34 +0300

    Add checks in card rules

--- src/domain/rules/card_rules.py
+++ src/domain/rules/card_rules.py
@@ -1,5 +1,32 @@
 from decimal import Decimal
 
+from fastapi import HTTPException, status
+
+from src.db.models.card import Card
+from src.domain.enums.card_status import CardStatus
+
+
 def validate_transaction_amount(amount: Decimal) -> None:
     if amount <= Decimal('0.00'):
         raise ValueError('Amount must be positive')
+
+def check_card_existence(card: Card | None):
+    if card is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Card not found'
+        )
+
+def check_card_is_active(card: Card):
+    if card.status == CardStatus.BLOCKED:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Card is already blocked'
+        )
+
+def check_card_is_blocked(card: Card):
+    if card.status != CardStatus.BLOCKED:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Card is not blocked'
+        )
