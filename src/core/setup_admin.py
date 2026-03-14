ffeed7674fbf675c06c84dee4d633c473469d59a
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-15 01:04:59 +0300

    Maka admin bootstrap async

--- src/core/setup_admin.py
+++ src/core/setup_admin.py
@@ -1,38 +1,41 @@
+import asyncio
 import logging
 
 from sqlalchemy import select
 
 from src.core.config import settings
+from src.core.security.hashing import hash_password
+from src.db.models.card import Card
 from src.db.models.user import User
 from src.db.session import SessionLocal
 from src.domain.enums.user_role import UserRole
 
+async def create_admin():
+    async with SessionLocal() as db:
+        try:
+            result = await db.execute(
+                select(User).where(User.role == UserRole.ADMIN)
+            )
+            admin_exists = result.scalar_one_or_none()
 
-def create_admin():
-    db = SessionLocal()
-    try:
-        admin_exists = db.execute(
-            select(User).where(User.role == UserRole.ADMIN)
-        ).scalar_one_or_none()
+            if admin_exists:
+                logging.info(f'Admin ({admin_exists.username}) already exists')
+                return
 
-        if admin_exists:
-            logging.info(f'Admin ({admin_exists.username}) already exists') 
-            return
+            admin = User(
+                username=settings.ADMIN,
+                hashed_password=hash_password(settings.ADMIN_PASSWORD),
+                role=UserRole.ADMIN
+            )
 
-        admin = User(
-            username=settings.ADMIN,
-            hashed_password=settings.ADMIN_PASSWORD,
-            role=UserRole.ADMIN
-        )
-        
-        db.add(admin)
-        db.commit()
-        logging.info(f'Admin ({admin}) created')
-        
-        
-    except:
-        logging.error(f'Error creating admin ({Exception})')
-        db.rollback()
-        
+            db.add(admin)
+            await db.commit()
+            logging.info(f'Admin ({admin.username}) created successfully')
+
+        except Exception as e:
+            await db.rollback()
+            logging.error(f'Error creating admin ({e})', exc_info=True)
+            await db.rollback()
+
 if __name__ == '__main__':
-    create_admin()
+    asyncio.run(create_admin())
