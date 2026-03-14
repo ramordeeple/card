b7bc75779e275ef89bc4595b1814951df137b4c2
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-14 08:24:29 +0300

    Refactor card router with issue func to contain encrypted number

--- src/api/routes/cards.py
+++ src/api/routes/cards.py
@@ -7,28 +7,36 @@ from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api import deps
+from src.core.security.crypto import encrypt_card_number
 from src.core.util.card_number_generator import generate_rand_card_number
 from src.db.models.card import Card
 from src.db.models.user import User
-from src.db.session import SessionLocal
-from src.domain.constants.card_constants import CARD_VALIDITY_YEARS
+from src.db.session import get_db
+from src.domain.constants.card_constants import CARD_VALIDITY_YEARS, CARD_MASK_VISIBLE_END
 from src.domain.enums.card_status import CardStatus
 from src.schemas.card import CardRead
 
-router = APIRouter(prefix='/cards', tags=['Cards'])
+router = APIRouter(prefix="/cards", tags=["Cards"])
 
-@router.post('/issue', response_model=CardRead, status_code=status.HTTP_201_CREATED)
+
+@router.post("/issue", response_model=CardRead, status_code=status.HTTP_201_CREATED)
 async def issue_card(
-        db: AsyncSession = Depends(SessionLocal), #TODO
-        current_user: User = Depends(deps.get_current_user)
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(deps.get_current_user),
 ):
+    generated_number = generate_rand_card_number()
+    encrypted_bytes = encrypt_card_number(generated_number)
+
     new_card = Card(
         id=uuid.uuid4(),
-        number_encrypted=generate_rand_card_number(),
+        number_encrypted=encrypted_bytes,
+        number_last4=generated_number[-CARD_MASK_VISIBLE_END:],
         owner_id=current_user.id,
-        expiration_date=(datetime.now() + timedelta(days=365 * CARD_VALIDITY_YEARS)).date(),
+        expiration_date=(
+            datetime.now() + timedelta(days=365 * CARD_VALIDITY_YEARS)
+        ).date(),
         status=CardStatus.ACTIVE,
-        balance=Decimal('0.00')
+        balance=Decimal("0.00"),
     )
 
     db.add(new_card)
