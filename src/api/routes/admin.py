86a06481ccf99306f0429ad96af5f7be5472c429
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-15 18:17:24 +0300

    Add response model for admin router

--- src/api/routes/admin.py
+++ src/api/routes/admin.py
@@ -1,38 +1,28 @@
+from typing import List
 from uuid import UUID
 
-from fastapi import Query, APIRouter
-from fastapi.params import Depends
+from fastapi import Query, APIRouter, Depends
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.db.models.card import Card
 from src.db.models.user import User
 from src.db.session import get_db
 from src.domain.enums.card_status import CardStatus
 from src.domain.rules import card_rules
+from src.schemas.card import CardRead, CardListResponse
 from src.services.card_service import CardService
 
 router = APIRouter(prefix='/admin', tags=['Admin Operations'])
 
-@router.get('/cards')
-async def get_all_cards(
-        db: AsyncSession = Depends(get_db),
-        limit: int = Query(10, ge=1, le=100, alias='limit'),
-        offset: int = Query(0, ge=0)
+@router.get("/cards", response_model=CardListResponse)
+async def get_all_cards_admin(
+    limit: int = 10,
+    offset: int = 0,
+    db: AsyncSession = Depends(get_db),
 ):
-    statement = select(Card).limit(limit).offset(offset)
-    result = await db.execute(statement)
-    cards = result.scalars().all()
-
-    total_result = await db.execute(select(func.count(Card.id)))
-
-    return {
-        'items': cards,
-        'total': total_result,
-        'offset': offset,
-        'limit': limit
-    }
-
+    return await CardService.get_cards(
+        db, owner_id=None, limit=limit, offset=offset
+    )
 
 @router.get('/users')
 async def get_all_users(
@@ -54,7 +44,7 @@ async def get_all_users(
         'total': total
     }
 
-@router.patch('/cards/{card_id}/unblock')
+@router.patch('/cards/{card_id}/unblock', response_model=CardRead)
 async def admin_unblock_card(
         card_id: UUID,
         db: AsyncSession = Depends(get_db),
@@ -76,7 +66,7 @@ async def admin_unblock_card(
         'status': card.status
     }
 
-@router.patch('/cards/{card_id}/block')
+@router.patch('/cards/{card_id}/block', response_model=CardRead)
 async def admin_block_card(
         card_id: UUID,
         db: AsyncSession = Depends(get_db),
@@ -98,7 +88,7 @@ async def admin_block_card(
         'status': card.status
     }
 
-@router.post("/users/{user_id}/cards", status_code=201)
+@router.post("/users/{user_id}/cards", response_model=CardRead)
 async def admin_create_card(
     user_id: UUID,
     db: AsyncSession = Depends(get_db),
